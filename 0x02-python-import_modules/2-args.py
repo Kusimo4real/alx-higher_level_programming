5037f50f3fe65d7d8087f12f61f1f0692f7ac9b0
Kusimo4real <abdulsemiukusimo@gmail.com>
2024-05-18 14:57:43 +0100

    a program that prints the m=number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,10 @@
 if __name__ == "__main__":
     import sys
     n = len(sys.argv[1:])
-    if n == 1:
-        print("0 argument.")
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("{} argument:".format(n))
     else:
         print("{} arguments:".format(n))
 

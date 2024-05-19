12326de04419744bab4e6e1eaf9840a40c4fde54
Kusimo4real <abdulsemiukusimo@gmail.com>
2024-05-19 06:26:32 -0700

    yeah

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-    a program that prints all the names deifned by the compiled module hidden_4.pyc
+    a program that prints all the names deifned by
+    the compiled module hidden_4.pyc
 """
-if __name__ == "__main":
+if __name__ == "__main__":
     import hidden_4
-    names=dir(hidden_4)
+    names = dir(hidden_4)
     for i in names:
-        if i.startwith("_"):
+        if i.startswith("_"):
             continue
         print(i)
-

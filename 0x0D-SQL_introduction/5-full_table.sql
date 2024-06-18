2547a383a5c8523303dcac173c2f30fe345ddcf2
Kusimo4real <kusimosemiu@gmail.com>
2024-06-18 04:03:37 +0100

    full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- a script that prints the full description of the table first_table from the database hbtn_0c_0
-
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS; WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW COLUMNS FROM first_table FROM hbtn_0c_0;

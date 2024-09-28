a7ff6fd3d07e9c5a64f77426009e5ec81f906d28
Kusimo4real <abdulsemiukusimo@gmail.com>
2024-09-28 15:47:07 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class square"""
+
+
+class Square:
+    """Represent a class, square"""
+
+    def __init__(self, size):
+        """Initialize a new square"""
+        __size = size

57353516fcfd6cad8ca30477cf7dd48dfac887d9
Kusimo4real <abdulsemiukusimo@gmail.com>
2025-01-28 18:49:50 +0100

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for number in range(0, 100):
     if number >= 99:
-        print("{}".format(number), end= "\n")
+        print("{}".format(number), end="\n")
     else:
         print("{:02d}".format(number), end=", ")

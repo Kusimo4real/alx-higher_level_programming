dc723406f66aae999cfb51a98ebfe9397372a6ab
Kusimo4real <abdulsemiukusimo@gmail.com>
2025-03-23 21:55:23 +0100

    print ascii in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,10 +2,7 @@
 
 def print_ascii():
     for i in range(122, 96, -1):
-        if i % 2 == 0:
-            print((chr(i)), end='')
-        else:
-            print((chr(i-32)), end='')
+        print((chr(i) if i % 2 == 0 else chr(i-32)), end='')
 
 
 print_ascii()

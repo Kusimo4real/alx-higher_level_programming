deea30304f04f1650009ec9728b16dfc796ac25c
Kusimo4real <abdulsemiukusimo@gmail.com>
2025-03-26 22:55:57 +0100

    added integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            print("{}".format((row[i])), end=' ')
-        print(''.format(''))
+            print("{:d}".format((row[i])), end=' ')
+    print(''.format(''))

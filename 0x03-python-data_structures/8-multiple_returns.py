72de2b565da561d437550829e50b1900f998a193
Kusimo4real <abdulsemiukusimo@gmail.com>
2025-04-03 00:45:15 +0100

    More returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+def multiple_returns(sentence):
+    if sentence == "":
+        first = None
+        return first
+    length = 0
+    for i in sentence:
+        length = length + 1
+    first = sentence[0]
+
+    return length, first

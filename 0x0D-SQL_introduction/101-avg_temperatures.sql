8a5591b0ab85d3231b9cd8baacc52d8a0d3e06d7
Ask4real <abdulsemiukusimo@gmail.com>
2025-10-19 20:35:40 +0100

    a script that displays the average temperature

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,5 @@
 --a script that displays the average temperature
-SELECT city, AVG(temperature) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
-

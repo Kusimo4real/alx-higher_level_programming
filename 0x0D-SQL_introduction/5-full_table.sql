460d193ae3ce988fd7d3056394a646a24a24c75c
Ask4real <abdulsemiukusimo@gmail.com>
2025-10-19 14:51:17 +0100

    a script that prints the full description of the table first_table from the database

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- a script that prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your mysql server
-SELECT COLUMN_NAME, DATA_TYPE
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SHOW CREATE TABLE first_table;
